b41c81728d7a2b07ca6293e91cf21dab3c31f6bf
PSCV2 <98485676+PSCV2@users.noreply.github.com>
2024-03-15 00:10:37 +0000

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,168 @@
+function myFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName('li');
+    
+    for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  }
+
+  filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+  
+}
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", " ");
+    this.className += " active";
+  });
+}
+
+
+class CitiesSlider extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.IMAGE_PARTS = 4;
+
+    this.changeTO = null;
+    this.AUTOCHANGE_TIME = 4000;
+
+    this.state = { activeSlide: -1, prevSlide: -1, sliderReady: false };
+  }
+
+  componentWillUnmount() {
+    window.clearTimeout(this.changeTO);
+  }
+
+  componentDidMount() {
+    this.runAutochangeTO();
+    setTimeout(() => {
+      this.setState({ activeSlide: 0, sliderReady: true });
+    }, 0);
+  }
+
+  runAutochangeTO() {
+    this.changeTO = setTimeout(() => {
+      this.changeSlides(1);
+      this.runAutochangeTO();
+    }, this.AUTOCHANGE_TIME);
+  }
+
+  changeSlides(change) {
+    window.clearTimeout(this.changeTO);
+    const { length } = this.props.slides;
+    const prevSlide = this.state.activeSlide;
+    let activeSlide = prevSlide + change;
+    if (activeSlide < 0) activeSlide = length - 1;
+    if (activeSlide >= length) activeSlide = 0;
+    this.setState({ activeSlide, prevSlide });
+  }
+
+  render() {
+    const { activeSlide, prevSlide, sliderReady } = this.state;
+    return (
+      React.createElement("div", { className: classNames('slider', { 's--ready': sliderReady }) },
+      React.createElement("p", { className: "slider__top-heading" }, "Travelers"),
+      React.createElement("div", { className: "slider__slides" },
+      this.props.slides.map((slide, index) =>
+      React.createElement("div", {
+        className: classNames('slider__slide', { 's--active': activeSlide === index, 's--prev': prevSlide === index }),
+        key: slide.city },
+
+      React.createElement("div", { className: "slider__slide-content" },
+      React.createElement("h3", { className: "slider__slide-subheading" }, slide.country || slide.city),
+      React.createElement("h2", { className: "slider__slide-heading" },
+      slide.city.split('').map(l => React.createElement("span", null, l))),
+
+      React.createElement("p", { className: "slider__slide-readmore" }, "read more")),
+
+      React.createElement("div", { className: "slider__slide-parts" },
+      [...Array(this.IMAGE_PARTS).fill()].map((x, i) =>
+      React.createElement("div", { className: "slider__slide-part", key: i },
+      React.createElement("div", { className: "slider__slide-part-inner", style: { backgroundImage: `url(${slide.img})` } }))))))),
+
+
+
+
+
+
+      React.createElement("div", { className: "slider__control", onClick: () => this.changeSlides(-1) }),
+      React.createElement("div", { className: "slider__control slider__control--right", onClick: () => this.changeSlides(1) })));
+
+
+  }}
+
+  window.onscroll = function() {scrollFunction()};
+
+
+const slides = [
+{
+  city: 'Inspired _By_Nature',
+  country: 'Feature walling / Ceilings / Furniture / Interiors / Exteriors',
+  img: '6.jpg' },
+
+{
+  city: 'Sliced Thin',
+  country: 'Take Your Home To the Next Level!',
+  img: '4.jpg' },
+
+{
+  city: 'Stone Sheets made of Natural Stone',
+  country: 'Exterior Walls',
+  img: '1.jpg' }];
+
+
+  function CartAdd(){
+    var price = element.getElementsByClassName("product-price-1")
+    var product = element.getElementsByClassName("product-name")
+
+    var item_list = []
+    item_list.push(product, price)
+    alert (item_list)
+    
+  }
+
+
+ReactDOM.render(React.createElement(CitiesSlider, { slides: slides }), document.querySelector('#app'));
